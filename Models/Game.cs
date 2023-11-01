5001473bf2ebd9be4542012058aacd4961971817
Jannick Mørup <jannickmorup@gmail.com>
2023-11-01 09:03:43 +0100

    Add project files.

---
+++ Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace GameCatalog.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Grenre { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}

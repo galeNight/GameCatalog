119136cb95eafeeb4a240f124e10c559b07ade6b
Jannick Mørup <jannickmorup@gmail.com>
2023-11-01 10:16:29 +0100

    moved code from index to gameclient and createt a new razor page editgame

---
+++ GameClient.cs
@@ -0,0 +1,40 @@
+﻿using GameCatalog.Models;
+
+namespace GameCatalog
+{
+    public static class GameClient
+    {
+         private static readonly List<Game> games = new()
+        {
+            new Game()
+            {
+                Id = 1,
+                Name = "Street Fighter II",
+                Grenre = "fightting",
+                Price = 19.99M,
+                ReleaseDate = new DateTime(1991,2,1)
+            },
+            new Game()
+            {
+                Id = 2,
+                Name = "Firnal Fantasy XIV",
+                Grenre = "Roleplaying",
+                Price = 59.99M,
+                ReleaseDate = new DateTime(2010,9,30)
+            },
+            new Game()
+            {
+                Id = 3,
+                Name = "Fifa 23",
+                Grenre = "Sports",
+                Price = 69.99M,
+                ReleaseDate = new DateTime(2022,9,27)
+            }
+        };
+
+        public static Game[] GetGames()
+        {
+            return games.ToArray();
+        }
+    }
+}

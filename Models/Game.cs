1f1e016b750c9d85006aec8557937f8d208f58e7
Jannick Mørup <jannickmorup@gmail.com>
2023-11-01 11:08:01 +0100

    add validation

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,10 +1,18 @@
-﻿namespace GameCatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameCatalog.Models
 {
     public class Game
     {
         public int Id { get; set; }
+
+        [Required]//validation
+        [StringLength(50)]//namelengh
         public required string Name { get; set; }
+        [Required]
+        [StringLength(20)]
         public required string Grenre { get; set; }
+        [Range(1,100)]//price range
         public decimal Price { get; set; }
         public DateTime ReleaseDate { get; set; }
     }
